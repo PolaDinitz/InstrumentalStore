02ff3e586dba3cc98b6e4047a06a1bbdedd63623
PolaDinitz <44160650+PolaDinitz@users.noreply.github.com>
2021-12-29 16:01:21 +0200

    create responsive navbar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { 
-  BrowserRouter, 
-  Routes,
-  Route,
-  Link
-} from 'react-router-dom';
-import { render } from '@testing-library/react';
+import React, { SyntheticEvent, useState } from 'react';
+
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav style={{ margin: 10 }}>
-        <Link to="/" style={{ padding: 5 }}>Home</Link>
-        <Link to="/about" style={{ padding: 5 }}>About</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <Navbar/>
   );
-}
-
-function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>Home View</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-    </div>
-  )
-}
-
-function About() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>About View</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-    </div>
-  )
 }
 
 export default App;
