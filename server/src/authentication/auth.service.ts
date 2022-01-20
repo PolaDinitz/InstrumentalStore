7ee0263cba751f6e1f766198b25b7dfcd9dc80af
Ran Biderman <bsuro10@gmail.com>
2022-01-21 00:01:54 +0200

    Added more fields to send to client when login

--- server/src/authentication/auth.service.ts
+++ server/src/authentication/auth.service.ts
@@ -23,6 +23,10 @@ export class AuthService {
       const payload = { email: user.email, sub: user.id, role: user.role};
       return {
         access_token: this.jwtService.sign(payload),
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        role: user.role
       };
     }
 }
