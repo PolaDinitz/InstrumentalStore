74c557b904ca3d9b40a84cd6de15a994e658138d
PolaDinitz <44160650+PolaDinitz@users.noreply.github.com>
2022-02-14 19:53:43 +0200

    added lodash and improved filter

--- client/src/components/Catalog/Catalog.tsx
+++ client/src/components/Catalog/Catalog.tsx
@@ -1,64 +1,97 @@
-import {Container, Divider, Grid, IconButton, InputBase, Paper, Typography} from "@mui/material";
-import {useDispatch, useSelector} from "react-redux";
-import {productsActions} from "../../redux/Product/product.actions";
+import _ from 'lodash';
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Container,
+  Divider,
+  Grid,
+  IconButton,
+  InputBase,
+  Paper,
+  Typography,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { productsActions } from "../../redux/Product/product.actions";
 import SingleProduct from "./SingleProduct/SingleProduct";
-import {Product} from "../../redux/Product/product.model";
+import { Product } from "../../redux/Product/product.model";
 import useUpdateEffect from "../../utils/use-update-effect";
-import {productsSelector} from "../../redux/Product/product.selector";
-import SearchIcon from '@mui/icons-material/Search';
-import {useState} from "react";
+import { productsSelector } from "../../redux/Product/product.selector";
+
 
 const Catalog = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useUpdateEffect(() => {
-        dispatch(productsActions.loadProducts());
-    },[])
+  useUpdateEffect(() => {
+    dispatch(productsActions.loadProducts());
+  }, []);
 
-    const allProducts: Product[] = useSelector(productsSelector.selectAll);
-    const [searchText, setSearchText] = useState('');
+  const allProducts: Product[] = useSelector(productsSelector.selectAll);
+  const [searchText, setSearchText] = useState("");
 
-    const handleSearch = (event: any) => {
-        setSearchText(event.target.value);
-    }
+  const handleSearch = (event: any) => {
+    setSearchText(event.target.value);
+  };
 
-    const filter = (product: Product) => {
-        return product.instrumentName.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()) ||
-            product.category.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())
-    }
-
+  const filteredProducts = allProducts.filter((product: Product) => {
     return (
-    <Container sx={{display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "20px" }}>
-        <Paper color="grey" elevation={0} sx={{ width: "100%", padding: "10px", margin: "10px"}}>
-            <Typography variant="h3" component="h3">
-                Instruments Catalog
-            </Typography>
-            <Typography variant="subtitle1" component="h6">
-                Browse, Click and Shop!
-            </Typography>
-        </Paper>
-        <Paper
-            component="form"
-            elevation={2}
-            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: "100%", marginBottom: "20px" }}
-        >
-            <InputBase
-                onKeyPress={e => e.key === 'Enter' && e.preventDefault()}
-                sx={{ ml: 1, flex: 1 }}
-                placeholder="Search"
-                onChange={handleSearch}
-            />
-            <IconButton sx={{ p: '10px' }} aria-label="search">
-                <SearchIcon />
-            </IconButton>
-        </Paper>
-        <Grid container spacing={4}>
-            {allProducts.filter(filter).map((product: Product) => (
-              <SingleProduct key={product._id} product={product} />
-            ))}
-        </Grid>
-    </Container>
+      product.instrumentName
+        .toLocaleLowerCase()
+        .includes(searchText.toLocaleLowerCase()) ||
+      product.category
+        .toLocaleLowerCase()
+        .includes(searchText.toLocaleLowerCase())
     );
+  });
+
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginBottom: "20px",
+      }}
+    >
+      <Paper
+        color="grey"
+        elevation={0}
+        sx={{ width: "100%", padding: "10px", margin: "10px" }}
+      >
+        <Typography variant="h3" component="h3">
+          Instruments Catalog
+        </Typography>
+        <Typography variant="subtitle1" component="h6">
+          Browse, Click and Shop!
+        </Typography>
+      </Paper>
+      <Paper
+        component="form"
+        elevation={2}
+        sx={{
+          p: "2px 4px",
+          display: "flex",
+          alignItems: "center",
+          width: "100%",
+          marginBottom: "20px",
+        }}
+      >
+        <InputBase
+          onKeyPress={(e) => e.key === "Enter" && e.preventDefault()}
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search"
+          onChange={handleSearch}
+        />
+        <IconButton sx={{ p: "10px" }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+      <Grid container spacing={4}>
+        {!_.isEmpty(filteredProducts) ? filteredProducts.map((product: Product) => (
+          <SingleProduct key={product._id} product={product} />
+        )) : <div style={{margin: "30px"}}>Oops, couldn't find any products</div>}
+      </Grid>
+    </Container>
+  );
 };
 
 export default Catalog;
