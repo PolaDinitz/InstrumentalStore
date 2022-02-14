604f9f65522c3c547bc09dae0cb6c4b064de13d2
PolaDinitz <44160650+PolaDinitz@users.noreply.github.com>
2022-02-14 20:51:53 +0200

    formatting and changing cart quantity

--- client/src/Routing.tsx
+++ client/src/Routing.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Catalog from './components/Catalog/Catalog';
-import Cart from './components/Cart/Cart';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Catalog from "./components/Catalog/Catalog";
+import Cart from "./components/Cart/Cart";
 import Navbar from "./Navbar";
 import Login from "./components/Login/Login";
 import ProductsDashboard from "./components/ProductsDashboard/ProductsDashboard";
@@ -12,45 +12,45 @@ import EditProduct from "./components/EditProduct/EditProduct";
 import OrdersHistory from "./components/OrdersHistory/OrdersHistory";
 
 const pages = [
-    {
-        name: "Catalog",
-        path: "/catalog",
-    }
+  {
+    name: "Catalog",
+    path: "/catalog",
+  },
 ];
 
 const Routing = () => {
-    return (
-        <Router>
-            <>
-                <Navbar pages={pages} />
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path='/' element={<PrivateRoute />}>
-                        <Route path='/' element={<Catalog />} />
-                    </Route>
-                    <Route path='/catalog' element={<PrivateRoute />}>
-                        <Route path='/catalog' element={<Catalog />} />
-                    </Route>
-                    <Route path='/cart' element={<PrivateRoute />}>
-                        <Route path='/cart' element={<Cart />} />
-                    </Route>
-                    <Route path='/products/dashboard' element={<PrivateRoute />}>
-                        <Route path='/products/dashboard' element={<ProductsDashboard />} />
-                    </Route>
-                    <Route path='/products/add' element={<PrivateRoute />}>
-                        <Route path='/products/add' element={<AddProduct />} />
-                    </Route>
-                    <Route path='/orders/history' element={<PrivateRoute />}>
-                        <Route path='/orders/history' element={<OrdersHistory />} />
-                    </Route>
-                    <Route path='/products/edit' element={<PrivateRoute/>}>
-                        <Route path='/products/edit' element={<EditProduct/>}/>
-                    </Route>
-                </Routes>
-            </>
-        </Router>
-    );
+  return (
+    <Router>
+      <>
+        <Navbar pages={pages} />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path="/" element={<Catalog />} />
+          </Route>
+          <Route path="/catalog" element={<PrivateRoute />}>
+            <Route path="/catalog" element={<Catalog />} />
+          </Route>
+          <Route path="/cart" element={<PrivateRoute />}>
+            <Route path="/cart" element={<Cart />} />
+          </Route>
+          <Route path="/products/dashboard" element={<PrivateRoute />}>
+            <Route path="/products/dashboard" element={<ProductsDashboard />} />
+          </Route>
+          <Route path="/products/add" element={<PrivateRoute />}>
+            <Route path="/products/add" element={<AddProduct />} />
+          </Route>
+          <Route path="/orders/history" element={<PrivateRoute />}>
+            <Route path="/orders/history" element={<OrdersHistory />} />
+          </Route>
+          <Route path="/products/edit" element={<PrivateRoute />}>
+            <Route path="/products/edit" element={<EditProduct />} />
+          </Route>
+        </Routes>
+      </>
+    </Router>
+  );
 };
 
 export default Routing;
